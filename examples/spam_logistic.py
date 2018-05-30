f181b52cf04b0b0eb757e854d550bbe016be3249
Havan <havanagrawal@gmail.com>
2018-05-29 22:32:12 -0700

    Add GridSearch example

--- examples/spam_logistic.py
+++ examples/spam_logistic.py
@@ -4,7 +4,10 @@ import sys
 import logging
 logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(process)d] %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
+import numpy as np
+
 from sklearn import preprocessing
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 
 _curdir = os.path.dirname(os.path.abspath(__file__))
@@ -21,6 +24,28 @@ def standardize(X_train, X_test):
 
     return (X_train, X_test)
 
+def train_spam_untuned(X_train, X_test, y_train, y_test):
+    clf = FGMClassifier(lmbda=0.1, max_iter=50, learning_rate='adaptive', eta=1)
+    logging.info("Training...")
+    clf.fit(X_train, y_train)
+
+    logging.info("Predicting...")
+    print("Training accuracy: ", clf.score(X_train, y_train))
+    print("Test accuracy: ", clf.score(X_test, y_test))
+
+def train_spam_tuned(X_train, X_test, y_train, y_test):
+    param_grid = {
+        'lmbda': np.linspace(0, 1, 10)
+    }
+    clf = GridSearchCV(FGMClassifier(), param_grid, verbose=2)
+
+    logging.info("Training...")
+    clf.fit(X_train, y_train)
+
+    logging.info("Predicting...")
+    print("Training accuracy: ", clf.score(X_train, y_train))
+    print("Test accuracy: ", clf.score(X_test, y_test))
+
 def main():
     logging.info("Loading data...")
     X_train, X_test, y_train, y_test = spam.load_dataset()
@@ -29,14 +54,11 @@ def main():
     X_train = preprocessing.add_dummy_feature(X_train)
     X_test = preprocessing.add_dummy_feature(X_test)
 
-    clf = FGMClassifier(lmbda=0.1, max_iter=20, learning_rate='adaptive', eta=1)
-    logging.info("Training...")
-    clf.fit(X_train, y_train)
+    logging.info("Training without tuning lambda...")
+    train_spam_untuned(X_train, X_test, y_train, y_test)
 
-    logging.info("Predicting...")
-    predictions = clf.predict(X_test)
-    print(accuracy_score(y_train, clf.predict(X_train)))
-    print(accuracy_score(y_test, clf.predict(X_test)))
+    logging.info("Training with GridSearchCV...")
+    train_spam_tuned(X_train, X_test, y_train, y_test)
 
 if __name__ == "__main__":
     main()
